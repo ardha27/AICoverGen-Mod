cc1e26beb673afd02c9f1ff0e5ebfdaf3387cc79
Ardha <ardhasemaranatha@gmail.com>
2024-08-10 02:56:18 +0800

    Update webui.py

--- src/webui.py
+++ src/webui.py
@@ -329,7 +329,6 @@ if __name__ == '__main__':
 
     app.launch(
         share=args.share_enabled,
-        enable_queue=True,
         server_name=None if not args.listen else (args.listen_host or '0.0.0.0'),
         server_port=args.listen_port,
     )
